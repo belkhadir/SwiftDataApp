bbbd8cdcc46076512747a0d62b28f5fd7d9cde2b
belkhadir <belkhadir.anas@gmail.com>
2025-07-12 11:17:42 +0100

    Initial Commit

---
+++ SwiftDataApp/SwiftDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataAppApp.swift
+//  SwiftDataApp
+//
+//  Created by Belkhadir Anas on 12/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
